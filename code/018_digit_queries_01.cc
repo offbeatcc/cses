d979726554698ff84659243ff11a439237cc2c14
Susam Pal <susam@susam.in>
2021-12-14 15:01:18 +0530

    Improve notation for Digit Queries solution

--- code/018_digit_queries_01.cc
+++ code/018_digit_queries_01.cc
@@ -1,33 +1,31 @@
 #include <iostream>
 #include <cmath>
 
-// Precompute a[n] = number of digits in concatenation of 1, 2, ..., 10^n - 1.
-void precompute_lengths(long long (&a)[18])
-{
-    a[0] = 0;
-    for (std::size_t n = 1; n < sizeof a / sizeof *a; ++n) {
-        // a[n] = a[n - 1] + 9 * n * 10^(n - 1).
-        a[n] = a[n - 1] + 9 * n * std::llround(std::pow(10, n - 1));
-    }
-}
-
 // Return the kth digit in concatenation of 1, 2, 3, ...
-int solve(long long k, long long (&a)[18])
+int solve(long long k)
 {
-    // Find n = number of digits in the integer at the kth place.
-    int n = 0;
-    while (a[n] < k) {
+    // Let a_i = number of digits in concatenation of 1, 2, ..., 10^i - 1.
+    // Find a_max = the largest a_i < k. Set n = max + 1.
+
+    // Note: n is now the width (number of digits) of the integer that
+    // the kth digit belongs to.
+
+    long long a_max = 0; // We save the maximum a_i that does not exceed k here.
+    long long a_next = 0; // We compute the next value of a_i and save it here.
+    int n = 0; // n = max + 1.
+
+    while (a_next < k) {
+        a_max = a_next;
         ++n;
+        // a[n] = a[n - 1] + 9n * 10^(n - 1).
+        a_next = a_next + 9 * n * std::llround(std::pow(10, n - 1));
     }
 
-    // Maximum (n - 1)th digit integer = 10^(n - 1) - 1.
-    long long m = std::llround(std::pow(10, n - 1)) - 1;
-
-    // The nth digit integer that ends on or before kth place.
-    long long p = m + (k - a[n - 1]) / n;
+    // The n-digit integer that ends on or before kth place.
+    long long p = std::llround(std::pow(10, n - 1)) - 1 + (k - a_max) / n;
 
     // Distance between the last digit of p and the kth digit.
-    int r = (k - a[n - 1]) % n;
+    int r = (k - a_max) % n;
 
     if (r == 0) {
         // The last digit of p.
@@ -43,13 +41,9 @@ int main()
     int q; // 1 <= q <= 1000.
     long long k; // 1 <= k <= 10^18.
 
-    long long a[18];
-    precompute_lengths(a);
-
     std::cin >> q;
     for (int i = 0; i < q; ++i) {
         std::cin >> k;
-        std::cout << solve(k, a) << '\n';
+        std::cout << solve(k) << '\n';
     }
-
 }
